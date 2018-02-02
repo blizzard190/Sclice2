38124b10d54b5acd55dd04bc0c836b2ad68d3029
blizzard190 <22256@ma-web.nl>
2018-02-02 08:47:13 +0100

    Alles

---
+++ Blocks/BlockHp.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockHp : MonoBehaviour {
+    private float hp = 3;
+    private SpriteRenderer spR;
+    public Sprite damage;
+    [SerializeField]
+    private ParticleSystem _particle;
+
+    private void Start()
+    {
+        spR = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "Player" || other.gameObject.tag == "Ground")
+        {
+            hp -= 1;
+        }
+    }
+    private void Update()
+    {
+        if(hp == 1)
+        {
+            spR.sprite = damage;
+        }
+        else if (hp <= 0)
+        {
+            var pSystem = Instantiate(_particle);
+            pSystem.transform.position = transform.position;
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Blocks/GlassBreak.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlassBreak : MonoBehaviour {
+
+    [SerializeField]
+    private ParticleSystem particle; 
+    private float hp = 1;
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player" || other.gameObject.tag == "Ground")
+        {
+            hp -= 1;
+        }
+    }
+    
+    void Update () {
+        if (hp <= 0)
+        {
+            var pSystem = Instantiate(particle);
+            pSystem.transform.position = transform.position;
+            Destroy(gameObject);
+        }
+    }
+}
